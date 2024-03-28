be25d13732a11ed8195310541469a7e4d48fbf22
FARHAN M HAMEED <122090335+farhanabhi@users.noreply.github.com>
2024-03-28 13:30:33 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: iPortfolio
-* Updated: Nov 17 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
@@ -260,3 +254,69 @@
   new PureCounter();
 
 })()
+
+(function ($) {
+  'use strict';
+  
+AOS.init({
+  once: true
+});
+
+  // ----------------------- 
+      // Progress Bar--------------------
+      // 
+      // 
+
+  $(window).on ('load', function (){ 
+        
+      $('.progress-bar').each(function(){
+              var width = $(this).data('percent');
+              $(this).css({'transition': 'width 3s'});
+              $(this).appear(function() {
+                  console.log('hello');
+                  $(this).css('width', width + '%');
+                  $(this).find('.count').countTo({
+                      from: 0,
+                      to: width,
+                      speed: 3000,
+                      refreshInterval: 50
+                  });
+              });
+          });
+      }); 
+
+  $('.owl-carousel').owlCarousel({
+      items:1,
+      loop:true,
+      autoplay:true,
+      dots:false,
+      autoplayTimeout:8000
+  });
+
+  // Shuffle js filter and masonry
+  var Shuffle = window.Shuffle;
+  var jQuery = window.jQuery;
+
+  var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+      itemSelector: '.shuffle-item',
+      buffer: 1
+  });
+
+  jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+      var input = evt.currentTarget;
+      if (input.checked) {
+          myShuffle.filter(input.value);
+      }
+  });
+
+   $('.portfolio-gallery').each(function () {
+      $(this).find('.popup-gallery').magnificPopup({
+          type: 'image',
+          gallery: {
+              enabled: true
+          }
+      });
+  });
+
+
+})(jQuery);
